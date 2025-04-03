b5b6a04c09d28201b71a1d621bcab7b1276ba06b
Ray Milkey <ray.milkey@gmail.com>
2025-04-03 11:15:45 -0700

    Wiper fixes for e2e test stability

--- pkg/wiper/grpc.go
+++ pkg/wiper/grpc.go
@@ -7,8 +7,11 @@ package wiper
 import (
 	"context"
 	catalogv3 "github.com/open-edge-platform/app-orch-catalog/pkg/api/catalog/v3"
+	"github.com/open-edge-platform/orch-library/go/dazl"
 	"google.golang.org/grpc/metadata"
 )
+
+var log = dazl.GetPackageLogger()
 
 // ProjectWiper is a tool for wiping data associated with a project
 type ProjectWiper interface {
@@ -38,7 +41,6 @@ func (w *grpcWiper) Wipe(ctx context.Context, projectUUID string) []error {
 	pctx := withActiveProjectID(ctx, projectUUID)
 
 	errors = append(errors, w.preparePackagesForDeletion(pctx)...)
-	errors = append(errors, w.prepareApplicationsForDeletion(pctx)...)
 
 	errors = append(errors, w.wipePackages(pctx)...)
 	errors = append(errors, w.wipeApplications(pctx)...)
@@ -50,14 +52,24 @@ func (w *grpcWiper) Wipe(ctx context.Context, projectUUID string) []error {
 // Sweeps through all packages, marking them as not deployed
 func (w *grpcWiper) preparePackagesForDeletion(ctx context.Context) []error {
 	var errors []error
-	resp, err := w.client.ListDeploymentPackages(ctx, &catalogv3.ListDeploymentPackagesRequest{})
-	if err != nil {
-		return append(errors, err)
-	}
-	for _, app := range resp.DeploymentPackages {
-		if err = w.preparePackageForDeletion(ctx, app.Name, app.Version); err != nil {
-			errors = append(errors, err)
+	hasMorePages := true
+	offset := int32(0)
+	for hasMorePages {
+		resp, err := w.client.ListDeploymentPackages(ctx, &catalogv3.ListDeploymentPackagesRequest{
+			PageSize: maxPageSize,
+			Offset:   offset,
+		})
+		if err != nil {
+			return append(errors, err)
 		}
+		for _, app := range resp.DeploymentPackages {
+			if err = w.preparePackageForDeletion(ctx, app.Name, app.Version); err != nil {
+				errors = append(errors, err)
+			}
+		}
+		hasMorePages = resp.TotalElements > offset+int32(len(resp.DeploymentPackages))
+		offset = offset + int32(len(resp.DeploymentPackages))
+		log.Debugf("Total elements: %d, offset: %d, len: %d hasMorePages: %t", resp.TotalElements, offset, len(resp.DeploymentPackages), hasMorePages)
 	}
 	return errors
 }
@@ -73,11 +85,11 @@ func (w *grpcWiper) preparePackageForDeletion(ctx context.Context, name string, 
 	// Update package to sever any dependencies from its point of view
 	pkg := gresp.DeploymentPackage
 	pkg.IsDeployed = false
-	pkg.Profiles = []*catalogv3.DeploymentProfile{}
 	pkg.ApplicationReferences = []*catalogv3.ApplicationReference{}
 	pkg.ApplicationDependencies = []*catalogv3.ApplicationDependency{}
 	pkg.DefaultNamespaces = map[string]string{}
 	pkg.DefaultProfileName = ""
+	pkg.Profiles = []*catalogv3.DeploymentProfile{}
 
 	if _, err = w.client.UpdateDeploymentPackage(ctx, &catalogv3.UpdateDeploymentPackageRequest{
 		DeploymentPackageName: name, Version: version, DeploymentPackage: pkg,
@@ -87,104 +99,92 @@ func (w *grpcWiper) preparePackageForDeletion(ctx context.Context, name string, 
 	return nil
 }
 
-// Sweeps through all applications, severing their dependencies on any deployment packages
-func (w *grpcWiper) prepareApplicationsForDeletion(ctx context.Context) []error {
-	var errors []error
-	resp, err := w.client.ListApplications(ctx, &catalogv3.ListApplicationsRequest{})
-	if err != nil {
-		return append(errors, err)
-	}
-	for _, app := range resp.Applications {
-		if err = w.prepareApplicationForDeletion(ctx, app.Name, app.Version); err != nil {
-			errors = append(errors, err)
-		}
-	}
-	return errors
-}
-
-func (w *grpcWiper) prepareApplicationForDeletion(ctx context.Context, name string, version string) error {
-	gresp, err := w.client.GetApplication(ctx, &catalogv3.GetApplicationRequest{
-		ApplicationName: name, Version: version,
-	})
-	if err != nil {
-		return err
-	}
-
-	// Update app to remove any profiles that might have dependencies on packages
-	app := gresp.Application
-	app.Profiles = []*catalogv3.Profile{}
-	app.DefaultProfileName = ""
-
-	if _, err = w.client.UpdateApplication(ctx, &catalogv3.UpdateApplicationRequest{
-		ApplicationName: name, Version: version, Application: app,
-	}); err != nil {
-		return err
-	}
-	return nil
-}
-
 func (w *grpcWiper) wipePackages(ctx context.Context) []error {
 	var errors []error
-	resp, err := w.client.ListDeploymentPackages(ctx, &catalogv3.ListDeploymentPackagesRequest{})
-	if err != nil {
-		return append(errors, err)
-	}
-	for _, pkg := range resp.DeploymentPackages {
-		if _, err = w.client.DeleteDeploymentPackage(ctx, &catalogv3.DeleteDeploymentPackageRequest{
-			DeploymentPackageName: pkg.Name,
-			Version:               pkg.Version,
-		}); err != nil {
-			errors = append(errors, err)
+	hasMorePages := true
+	for hasMorePages {
+		resp, err := w.client.ListDeploymentPackages(ctx, &catalogv3.ListDeploymentPackagesRequest{PageSize: maxPageSize})
+		if err != nil {
+			return append(errors, err)
 		}
+		for _, pkg := range resp.DeploymentPackages {
+			log.Debugf("Deleting deployment package %s:%s", pkg.Name, pkg.Version)
+			if _, err = w.client.DeleteDeploymentPackage(ctx, &catalogv3.DeleteDeploymentPackageRequest{
+				DeploymentPackageName: pkg.Name,
+				Version:               pkg.Version,
+			}); err != nil {
+				errors = append(errors, err)
+			}
+		}
+		hasMorePages = resp.TotalElements > int32(len(resp.DeploymentPackages))
+		log.Debugf("Total elements: %d, len: %d hasMorePages: %t", resp.TotalElements, len(resp.DeploymentPackages), hasMorePages)
 	}
 	return errors
 }
 
 func (w *grpcWiper) wipeApplications(ctx context.Context) []error {
 	var errors []error
-	resp, err := w.client.ListApplications(ctx, &catalogv3.ListApplicationsRequest{})
-	if err != nil {
-		return append(errors, err)
-	}
-	for _, app := range resp.Applications {
-		if _, err = w.client.DeleteApplication(ctx, &catalogv3.DeleteApplicationRequest{
-			ApplicationName: app.Name,
-			Version:         app.Version,
-		}); err != nil {
-			errors = append(errors, err)
+	hasMorePages := true
+	for hasMorePages {
+		resp, err := w.client.ListApplications(ctx, &catalogv3.ListApplicationsRequest{PageSize: maxPageSize})
+		if err != nil {
+			return append(errors, err)
 		}
+		for _, app := range resp.Applications {
+			log.Debugf("Deleting application %s:%s", app.Name, app.Version)
+			if _, err = w.client.DeleteApplication(ctx, &catalogv3.DeleteApplicationRequest{
+				ApplicationName: app.Name,
+				Version:         app.Version,
+			}); err != nil {
+				errors = append(errors, err)
+			}
+		}
+		hasMorePages = resp.TotalElements > int32(len(resp.Applications))
+		log.Debugf("Total elements: %d, len: %d hasMorePages: %t", resp.TotalElements, len(resp.Applications), hasMorePages)
 	}
 	return errors
 }
 
 func (w *grpcWiper) wipeArtifacts(ctx context.Context) []error {
 	var errors []error
-	resp, err := w.client.ListArtifacts(ctx, &catalogv3.ListArtifactsRequest{})
-	if err != nil {
-		return append(errors, err)
-	}
-	for _, artifact := range resp.Artifacts {
-		if _, err = w.client.DeleteArtifact(ctx, &catalogv3.DeleteArtifactRequest{
-			ArtifactName: artifact.Name,
-		}); err != nil {
-			errors = append(errors, err)
+	hasMorePages := true
+	for hasMorePages {
+		resp, err := w.client.ListArtifacts(ctx, &catalogv3.ListArtifactsRequest{PageSize: maxPageSize})
+		if err != nil {
+			return append(errors, err)
 		}
+		for _, artifact := range resp.Artifacts {
+			log.Debugf("Deleting artifact %s", artifact.Name)
+			if _, err = w.client.DeleteArtifact(ctx, &catalogv3.DeleteArtifactRequest{
+				ArtifactName: artifact.Name,
+			}); err != nil {
+				errors = append(errors, err)
+			}
+		}
+		hasMorePages = resp.TotalElements > int32(len(resp.Artifacts))
+		log.Debugf("Total elements: %d, len: %d hasMorePages: %t", resp.TotalElements, len(resp.Artifacts), hasMorePages)
 	}
 	return errors
 }
 
 func (w *grpcWiper) wipeRegistries(ctx context.Context) []error {
 	var errors []error
-	resp, err := w.client.ListRegistries(ctx, &catalogv3.ListRegistriesRequest{})
-	if err != nil {
-		return append(errors, err)
-	}
-	for _, registry := range resp.Registries {
-		if _, err = w.client.DeleteRegistry(ctx, &catalogv3.DeleteRegistryRequest{
-			RegistryName: registry.Name,
-		}); err != nil {
-			errors = append(errors, err)
+	hasMorePages := true
+	for hasMorePages {
+		resp, err := w.client.ListRegistries(ctx, &catalogv3.ListRegistriesRequest{PageSize: maxPageSize})
+		if err != nil {
+			return append(errors, err)
 		}
+		for _, registry := range resp.Registries {
+			log.Debugf("Deleting registry %s", registry.Name)
+			if _, err = w.client.DeleteRegistry(ctx, &catalogv3.DeleteRegistryRequest{
+				RegistryName: registry.Name,
+			}); err != nil {
+				errors = append(errors, err)
+			}
+		}
+		hasMorePages = resp.TotalElements > int32(len(resp.Registries))
+		log.Debugf("Total elements: %d, len: %d hasMorePages: %t", resp.TotalElements, len(resp.Registries), hasMorePages)
 	}
 	return errors
 }
