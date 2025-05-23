8bd6a8ae263f9efe14aa0a4a4e40249550510418
Baker, Scott <scott.baker@intel.com>
2025-05-23 11:06:43 -0700

    add dp-to-helm test

---
+++ test/cmdline/dp_to_helm_test.go
@@ -0,0 +1,69 @@
+// SPDX-FileCopyrightText: (C) 2023-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package restapi
+
+import (
+	// Standard library imports
+
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+	// Third-party imports
+)
+
+const (
+	doToHelmTool = "../../build/_output/dp-to-helm"
+)
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetOutput(os.Stdout)
+}
+
+func (s *TestSuite) runDpToHelm(args ...string) (string, string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	log.Printf("Executing command: %s %v", doToHelmTool, args)
+
+	c := exec.CommandContext(ctx, doToHelmTool, args...)
+
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
+	c.Stdout = &stdout
+	c.Stderr = &stderr
+
+	err := c.Run()
+
+	outStr := stdout.String()
+	errStr := stderr.String()
+	return outStr, errStr, err
+}
+
+func (s *TestSuite) TestDpToHelmGoodPackage() {
+	tempDir, err := os.MkdirTemp("", "catalog_test_*")
+	s.Require().NoError(err)
+	defer os.RemoveAll(tempDir)
+
+	stdout, stderr, err := s.runDpToHelm(wordpressDir, "-o", tempDir)
+	s.NoError(err, "Expected no error when running catalog-schema on a good package")
+	s.Equal("", stderr, "Expected no error output when running catalog-schema on a good package")
+
+	expected := fmt.Sprintf("helm install test-wordpress https://charts.bitnami.com/bitnami/wordpress --version 19.4.3 --namespace default -f %s/test-wordpress-testing.yaml", tempDir)
+	s.Contains(stdout, expected, "Expected stdout to contain helm install command")
+
+}
+
+func (s *TestSuite) TestDpToHelmBadPackageMissingAppName() {
+	stdout, stderr, err := s.runDpToHelm(wordpressMissingAppDir)
+	s.Error(err, "Expected no error when running catalog-schema on a good package")
+	s.Equal("", stdout, "Expected no stdout output when running catalog-schema on a good package")
+	s.Contains(stderr, "InvalidArgument", "Expected stdout to contain 'InvalidArgument' when running catalog-schema on a bad package")
+}
