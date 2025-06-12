233e871c491d3e540fd5a56fae6a18ad8c142d9f
adibrastegarnia <adib.rastegarnia@intel.com>
2025-06-12 13:34:16 -0700

    fix

--- test/utils/methods/methods.go
+++ test/utils/methods/methods.go
@@ -345,13 +345,13 @@ func (c *CatalogClient) GetRegistries() []types.Registry {
 	return regs
 }
 
-func MakeHTTPRequest(method, endpoint string, body io.Reader) (*http.Response, error) {
-	requestURL := fmt.Sprintf("%s%s", s.catalogClient.CatalogRESTServerUrl, endpoint)
+func (c *CatalogClient) MakeHTTPRequest(method, endpoint string, body io.Reader) (*http.Response, error) {
+	requestURL := fmt.Sprintf("%s%s", c.CatalogRESTServerUrl, endpoint)
 	req, err := http.NewRequest(method, requestURL, body)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create HTTP request: %w", err)
 	}
-	auth.AddRestAuthHeader(req, s.catalogClient.Token, s.catalogClient.ProjectID)
+	auth.AddRestAuthHeader(req, c.Token, c.ProjectID)
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("failed to execute HTTP request: %w", err)
@@ -359,13 +359,13 @@ func MakeHTTPRequest(method, endpoint string, body io.Reader) (*http.Response, e
 	return res, nil
 }
 
-func MakeHTTPRequestWithQuery(method, endpoint string, queryParams map[string]string) (*http.Response, error) {
-	requestURL := fmt.Sprintf("%s%s", s.catalogClient.CatalogRESTServerUrl, endpoint)
+func (c *CatalogClient) MakeHTTPRequestWithQuery(method, endpoint string, queryParams map[string]string) (*http.Response, error) {
+	requestURL := fmt.Sprintf("%s%s", c.CatalogRESTServerUrl, endpoint)
 	req, err := http.NewRequest(method, requestURL, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create HTTP request: %w", err)
 	}
-	auth.AddRestAuthHeader(req, s.catalogClient.Token, s.catalogClient.ProjectID)
+	auth.AddRestAuthHeader(req, c.Token, c.ProjectID)
 	query := req.URL.Query()
 	for key, value := range queryParams {
 		query.Add(key, value)
